e0084af0f71725dd1377ac07ac0b953f6c458bcf
asafharel <asaf.harel@me.com>
2021-06-10 15:52:41 +0300

    fixed self edges

--- src/Graph.java
+++ src/Graph.java
@@ -61,7 +61,7 @@ public class Graph {
     public boolean addEdge(int node1_id, int node2_id) {
         ExtendedNode node1 = this.table.get(node1_id);
         ExtendedNode node2 = this.table.get(node2_id);
-        if (node1 == null || node2 == null) {
+        if (node1 == null || node2 == null || node1==node2) {
             return false;
         }
         NeighbouringNodes neighbours = new NeighbouringNodes(node1, node2);
